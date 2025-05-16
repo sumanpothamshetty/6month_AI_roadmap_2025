eae6e9d22c98d65760d67e58008731be83535bcd
sumanpothamshetty <83535766+sumanpothamshetty@users.noreply.github.com>
2025-05-16 21:02:07 +0530

    Add files via upload

---
+++ first prg.py
@@ -0,0 +1,7 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 16:30:00) [MSC v.1900 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print "hello world"
+SyntaxError: Missing parentheses in call to 'print'. Did you mean print("hello world")?
+>>> print ("hello world")
+hello world
+>>> 
